73662b92bab325b538b2fe3c8933351f8dab4f3c
Shashi Kumar <shashiessp@gmail.com>
2017-03-10 17:02:05 +0530

    minor fixes

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -3,6 +3,6 @@ from django.views.generic import ListView
 from blogapp.models import Post
 
 urlpatterns = [
-	url(r'^', ListView._asView(queryset = Post.objects.all().order_by("-created")[:2], 
+	url(r'^', ListView.as_view(queryset = Post.objects.all().order_by("-created")[:2], 
 								template_name = "blog.html")),
 ]
